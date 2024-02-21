07e7b3f4bf49981df8b461a2bd0649b3c1998ff3
Gabe Harvey <harvey7@gmail.com>
2024-02-21 11:32:13 -0600

    Added MYSQL Connection to server.js

--- server.js
+++ server.js
@@ -2,3 +2,12 @@
 const mySQL = require('mysql12');
 const cFonts = require('cfonts');
 const inquirer = require('inquirer');
+
+// Establish MYSQL Connection
+const connection = mySQL.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: '',
+    database: 'emptrack_db',
+});
