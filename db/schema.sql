8fa6ce10453cabb2bc0911b9fa329ab400d82d31
Gabe Harvey <harvey7@gmail.com>
2024-02-21 13:28:36 -0600

    Added Role Table Command to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,14 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL,
 );
+
+-- Create Table for Role
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
