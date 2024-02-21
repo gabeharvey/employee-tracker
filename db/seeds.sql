e136f682fd7711d19c70335b37d9964e2af2015c
Gabe Harvey <harvey7@gmail.com>
2024-02-21 13:51:04 -0600

    Added Department Data Insert in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+-- Place Data into Department Table
+INSERT INTO department (department_name)
+VALUES
+('Asset Management'),
+('Board of Directors'),
+('Public Relations'),
+('Finance'),
+('Human Resources'),
+('Investor Relations'),
+('Marketing'),
+('Legal'),
+('Quality Assurance'),
+('Social Media'),
+('Sales'),
+('Information Technology')
+
