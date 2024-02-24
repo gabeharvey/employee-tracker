200aaa33ece845aeb9ff66fd6c1937c9ef3c885c
Gabe Harvey <harvey7@gmail.com>
2024-02-24 13:20:59 -0600

    Fixed Bug in View All Employees Function in server.js

--- server.js
+++ server.js
@@ -126,7 +126,13 @@ function viewAllRoles() {
 
 // Allows View of All Employees
 function viewAllEmployees() {
-    const query = "SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager_name FROM employee e LEFT JOIN roles r ON e.role_id = r.id LEFT JOIN roles r ON e.role_id = r.id LEFT JOIN employee m ON e.manager_id = m.id;";
+    const query = `
+    SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+    FROM employee e
+    LEFT JOIN roles r ON e.role_id = r.id
+    LEFT JOIN department d ON r.department_id = d.id
+    LEFT JOIN employee m ON e.manager_id = m.id;
+    `;
     connection.query(query, (err, res) => {
         if (err) throw err;
         console.table(res);
