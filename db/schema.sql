1654a4c653614a35708c0e28282f2134a6e8545a
Gabe Harvey <harvey7@gmail.com>
2024-02-24 11:44:59 -0600

    Fixed Syntax Errors in schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,12 +8,14 @@ CREATE DATABASE emptrack_db;
 USE emptrack_db;
 
 -- Create Table for Department
+DROP TABLE IF EXISTS department;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL
 );
 
 -- Create Table for Role
+DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30) NOT NULL,
@@ -21,14 +23,15 @@ CREATE TABLE roles (
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL,
+    ON DELETE SET NULL
 );
 
 -- Create Table for Employee
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
+    manager_id INT
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -14,7 +14,7 @@ VALUES
 ("Sales"),
 ("Information Technology");
 
--- Place Data into Role Table
+-- -- Place Data into Role Table
 INSERT INTO roles (title, salary, department_id)
 VALUES
 ("CEO", 800000.00, 1),
@@ -24,13 +24,13 @@ VALUES
 ("HR Director", 300000.00, 5),
 ("Investor Relations Lead", 250000.00, 6),
 ("Marketing Director", 400000.00, 7),
-("Legal Director" 650000.00, 8),
+("Legal Director", 650000.00, 8),
 ("QA Director", 375000.00, 9),
 ("Social Media Lead", 385000.00, 10),
 ("Sales Manager", 155000.00, 11),
 ("IT Director", 900000.00, 12);
 
--- Place Data into Employee Table
+-- -- -- Place Data into Employee Table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ("Michael", "Scott", 1, 1),
